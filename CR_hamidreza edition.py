3c68b85d6113d73c01fa9f54c926cf76278a5904
Hamidreza Sanaee <34958824+HamidrezaSK@users.noreply.github.com>
2018-01-20 21:53:01 +0330

    drag and drop prototype
    without precise areas

---
+++ CR_hamidreza edition.py
@@ -0,0 +1,37 @@
+import pygame,sys
+
+pygame.init()
+windowWidth = 640
+windowHeight = 640
+first_X = 200
+first_Y = 590
+reverse_V = 10
+card_position = [200,590]
+surface = pygame.display.set_mode((windowWidth, windowHeight))
+pygame.display.set_caption('Drag and Drop!')
+onclick = False
+diffs = [0,0]
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		#elif event.type == pygame.MOUSEBUTTONUP:
+			#card_position = [pygame.mouse.get_pos()[0],pygame.mouse.get_pos()[1]]
+	if pygame.mouse.get_pressed()[0] and card_position[0]<pygame.mouse.get_pos()[0]<card_position[0]+50 and card_position[1]<pygame.mouse.get_pos()[1]<card_position[1]+50:
+		onclick = True
+		diffs = [card_position[0]-pygame.mouse.get_pos()[0],card_position[1]-pygame.mouse.get_pos()[1]]
+	if not pygame.mouse.get_pressed()[0]:
+		onclick = False
+	if onclick:
+		card_position = [pygame.mouse.get_pos()[0]+diffs[0],pygame.mouse.get_pos()[1]+diffs[1]]
+		pygame.draw.rect(surface,(255,0,0),(card_position[0],card_position[1],50,50))
+	if not onclick and  first_X-5<=card_position[0] <= first_X+5 and first_Y-5<=card_position[1] <= first_Y+5:
+		pygame.draw.rect(surface,(255,0,0),(card_position[0],card_position[1],50,50))
+	elif not onclick and not(card_position[0] == first_X and card_position[1] == first_Y):
+		card_position[0] = card_position[0] + reverse_V*(first_X - card_position[0])/((first_X - card_position[0])**2 + (first_Y - card_position[1])**2)**0.5
+		card_position[1] = card_position[1] + reverse_V*(first_Y - card_position[1])/((first_X - card_position[0])**2 + (first_Y - card_position[1])**2)**0.5
+
+	surface.fill((0,0,0))
+	pygame.draw.rect(surface,(255,0,0),(card_position[0],card_position[1],50,50))
+	pygame.display.update()
